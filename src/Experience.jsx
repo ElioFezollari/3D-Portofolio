c0ff43e755a455df0e6bac999d383d4fd06b2e08
ElioFezollari <fezollarielio@gmail.com>
2024-09-12 16:33:18 -0400

    Init commit

---
+++ src/Experience.jsx
@@ -0,0 +1,14 @@
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import Room from './Room'
+export default function Experience()
+{
+    return <>
+
+        <Perf position="top-left" />
+        <OrbitControls makeDefault />
+
+        <Room/>
+
+    </>
+}
---
+++ src/Room.jsx
@@ -0,0 +1,18 @@
+import { useGLTF } from "@react-three/drei";
+import { TextureLoader } from 'three'
+import { useFrame, useLoader } from '@react-three/fiber'
+
+export default function Room(){
+    // useFrame((state) => {
+    //     console.log(state.camera.position)
+    // })
+    const texture = useLoader(TextureLoader,'./bakedRoom.jpg')
+    texture.flipY = false
+    const {nodes} = useGLTF('./room.glb')
+    return( 
+        <mesh geometry={nodes.baked.geometry}>
+            
+            <meshBasicMaterial map={texture}></meshBasicMaterial>
+        </mesh>)
+    
+}
