728d6f4a048e6c31385bcf795749dec4dc840a5c
ElioFezollari <fezollarielio@gmail.com>
2024-10-23 01:47:36 -0400

    Populated PopUp Data, changed how card looks

--- src/Pages/Additions/PopUp.jsx
+++ src/Pages/Additions/PopUp.jsx
@@ -7,12 +7,43 @@ const PopUp = () => {
   return (
     popUpInfo && (
       <div className="pop-up" style={{ backgroundColor: popUpInfo.color }}>
-      <button onClick={()=>setPopUpInfo(null)} className="close-button">X</button>
+        <button onClick={() => setPopUpInfo(null)} className="close-button">
+          X
+        </button>
         <div className="image-div"></div>
-        <div className="image-container">
-          <img src={`/images/indicator_images/${popUpInfo.img}`} alt="" />
+        <div className="main-content-div">
+          <div className="image-container">
+            <img
+              src={`/images/indicator_images/${popUpInfo.img}`}
+              alt={popUpContext.imgAlt}
+            />
+          </div>
+          <div className="text-container">
+            <h1>{popUpInfo.title}</h1>
+            <div className="pop-up-info">
+              <p>
+                <b>Description:</b> {popUpInfo.description}
+              </p>
+              <p>
+                <b>Years of Experience:</b> {popUpInfo.yoe}
+              </p>
+              <p>
+                <b>A few of my skills:</b> {popUpInfo.skills}
+              </p>
+              {popUpInfo.ecosystem && <>
+                <div className="ecosystem-header">
+                <p><b>Some related technologies I'm proficient in:</b></p>
+                </div>
+                <div className="ecosystem-div">
+                {popUpInfo.ecosystem.map((tech, index) => (
+                  <img key={index} src={`/images/indicator_images/${tech.img}`} alt={tech.alt} />
+                ))}
+              </div>
+              </>
+             }
+            </div>
+          </div>
         </div>
-        <div className="content-container"></div>
       </div>
     )
   );
